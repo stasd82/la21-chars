18d6727c26d8ea8e800eb48fb11e3e956a63a51b
sta <stasd82@yandex.ru>
2022-06-27 00:50:36 +0300

    Add configuration support

--- app/services/chars-api/main.go
+++ app/services/chars-api/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"errors"
+	"expvar"
 	"fmt"
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
+	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"github.com/stasd82/la21-chars/foundation/logger"
+	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 )
+
+var build = "develop"
 
 func main() {
 
@@ -30,7 +38,59 @@ func main() {
 
 func run(sl *zap.SugaredLogger) error {
 
-	sl.Infow("It's works!")
+	// -----------------------------------------------------------
+	// GOMAXPROCS
+
+	options := maxprocs.Logger(sl.Infof)
+
+	if _, err := maxprocs.Set(options); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+	sl.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// -----------------------------------------------------------
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "Web service in Go using Kubernetes",
+		},
+	}
+
+	const prefix = "CHARS"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// -----------------------------------------------------------
+	// App Starting
+
+	sl.Infow("starting service", "version", build)
+	defer sl.Infow("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	sl.Infow("startup", "config", out)
+
+	expvar.NewString("build").Set(build)
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/stasd82/la21-chars
 
 go 1.18
 
-require go.uber.org/zap v1.21.0
+require (
+	github.com/ardanlabs/conf/v3 v3.1.2
+	go.uber.org/automaxprocs v1.5.1
+	go.uber.org/zap v1.21.0
+)
 
 require (
 	go.uber.org/atomic v1.7.0 // indirect
