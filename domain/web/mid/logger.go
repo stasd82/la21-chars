665e50dcdc58b39ef358b92813b794c7db85026e
sta <stasd82@yandex.ru>
2022-06-29 14:41:32 +0300

    Improve loggin by using Context and bump Tux to v0.0.7

--- domain/web/mid/logger.go
+++ domain/web/mid/logger.go
@@ -3,6 +3,7 @@ package mid
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/stasd82/tux"
 	"go.uber.org/zap"
@@ -13,11 +14,18 @@ func Logger(sl *zap.SugaredLogger) (c tux.Circuit) {
 
 		out = func(ctx context.Context, w http.ResponseWriter, r *http.Request) (err error) {
 
-			sl.Infow("request started")
+			v, err := tux.GetValues(ctx)
+			if err != nil {
+				return
+			}
+
+			sl.Infow("request started", "traceid", v.TraceID, "method", r.Method, "path", r.URL.Path,
+				"remoteaddr", r.RemoteAddr)
 			{
 				err = in(ctx, w, r)
 			}
-			sl.Infow("request completed")
+			sl.Infow("request completed", "traceid", v.TraceID, "method", r.Method, "path", r.URL.Path,
+				"remoteaddr", r.RemoteAddr, "statuscode", v.StatusCode, "since", time.Since(v.Now))
 
 			return
 		}
--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.18
 
 require (
 	github.com/ardanlabs/conf/v3 v3.1.2
-	github.com/stasd82/tux v0.0.5
+	github.com/stasd82/tux v0.0.7
 	go.uber.org/automaxprocs v1.5.1
 	go.uber.org/zap v1.21.0
 )
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/uptrace/bunrouter v1.0.17 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
